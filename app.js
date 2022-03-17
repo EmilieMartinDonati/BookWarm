076883af78f86a6220aa46472591235c05cf2a12
Régis Martin <regis.martinravel@gmail.com>
2022-03-17 22:01:55 +0100

    heroku

--- app.js
+++ app.js
@@ -46,82 +46,82 @@ app.use(
 
 // Implementing google signUp
 
-app.use(passport.initialize()); // init passport on every route call
-app.use(passport.session());
+// app.use(passport.initialize()); // init passport on every route call
+// app.use(passport.session());
 
-authUser = (request, accessToken, refreshToken, profile, done) => {
-  return done(null, profile);
-}
+// authUser = (request, accessToken, refreshToken, profile, done) => {
+//   return done(null, profile);
+// }
 
-const GOOGLE_CLIENT_ID = "331250048962-mvgeq323d1bjiu11n4hdemq66q2ln0ak.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+// const GOOGLE_CLIENT_ID = "331250048962-mvgeq323d1bjiu11n4hdemq66q2ln0ak.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_SECRET = "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";
 
-// Use "GoogleStrategy" as the Authentication Strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
-      passReqToCallback: true,
-    },
-    authUser
-  )
-);
+// // Use "GoogleStrategy" as the Authentication Strategy
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: GOOGLE_CLIENT_ID,
+//       clientSecret: GOOGLE_CLIENT_SECRET,
+//       callbackURL: "http://localhost:3000/auth/google/callback",
+//       passReqToCallback: true,
+//     },
+//     authUser
+//   )
+// );
 
-passport.serializeUser((user, done) => {
-  console.log(`\n--------> Serialize User:`);
-  console.log(user);
-  // The USER object is the "authenticated user" from the done() in authUser function.
-  // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.
+// passport.serializeUser((user, done) => {
+//   console.log(`\n--------> Serialize User:`);
+//   console.log(user);
+//   // The USER object is the "authenticated user" from the done() in authUser function.
+//   // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.
 
-  done(null, user);
-});
+//   done(null, user);
+// });
 
-passport.deserializeUser((user, done) => {
-  console.log("\n--------- Deserialized User:");
-  console.log(user);
-  // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
-  // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+// passport.deserializeUser((user, done) => {
+//   console.log("\n--------- Deserialized User:");
+//   console.log(user);
+//   // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
+//   // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
 
-  done(null, user);
-});
+//   done(null, user);
+// });
 
-// console.log() values of "req.session" and "req.user" so we can see what is happening during Google Authentication
-let count = 1
-showlogs = (req, res, next) => {
-    console.log("\n==============================")
-    console.log(`------------>  ${count++}`)
+// // console.log() values of "req.session" and "req.user" so we can see what is happening during Google Authentication
+// let count = 1
+// showlogs = (req, res, next) => {
+//     console.log("\n==============================")
+//     console.log(`------------>  ${count++}`)
 
-    console.log(`\n req.session.passport -------> `)
-    console.log(req.session.passport)
+//     console.log(`\n req.session.passport -------> `)
+//     console.log(req.session.passport)
   
-    console.log(`\n req.user -------> `) 
-    console.log(req.user) 
+//     console.log(`\n req.user -------> `) 
+//     console.log(req.user) 
   
-    console.log("\n Session and Cookie")
-    console.log(`req.session.id -------> ${req.session.id}`) 
-    console.log(`req.session.cookie -------> `) 
-    console.log(req.session.cookie) 
+//     console.log("\n Session and Cookie")
+//     console.log(`req.session.id -------> ${req.session.id}`) 
+//     console.log(`req.session.cookie -------> `) 
+//     console.log(req.session.cookie) 
   
-    console.log("===========================================\n")
+//     console.log("===========================================\n")
 
-    next()
-}
+//     next()
+// }
 
-app.use(showlogs);
+// app.use(showlogs);
 
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope:
-      [ 'email', 'profile' ] }
-));
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope:
+//       [ 'email', 'profile' ] }
+// ));
 
-app.get('/auth/google/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-}));
+// app.get('/auth/google/callback',
+//     passport.authenticate( 'google', {
+//         successRedirect: '/',
+//         failureRedirect: '/login',
+// }));
 
 
 
